b41dea6e283e00bf0c11fbcb5c7efc7f938c7100
Anish20262704 <152880188+Anish20262704@users.noreply.github.com>
2025-04-02 12:09:41 +0000

    Comaprision keye h

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==2);
+// console.log(2!=1);
+
+// console.log("2">1);//yha conversion hone lag jata h basically 
+// console.log("02">1);//J.S "2" ko integer me change kardega already fir Comparision hoga saab ka to yha answer predictable nhi hota h to jaab bhi comparision karo to dhyan dena ke same dataType ko he Compare kare Wo basically TypeScript of J.S me Likha Hua h Already 
+
+
+console.log(null >0);
+console.log(null == 0);//yha kabhi null NaN me convert ho jata h aur kabhi 0 
+console.log(null >=0);//yha null Convert Ho jayega jO J.S me hota h fir Compare Hojayega
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//*** === operator
+console.log("2" === 2);//yha Convesrion Strictly Check Hoga basically yha Data ko he nhi balki uske DataType ko bhi check karta h === h to conversion hoga he nhi yha datatypes Dekhana ha basically 
+
+
+
