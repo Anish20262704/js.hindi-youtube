75505182e389552c488fa1e4c0166008d2ddec94
Anish20262704 <152880188+Anish20262704@users.noreply.github.com>
2025-04-02 05:24:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all JS code as newer version
+
+//alert(3+3) // We are using nodejs,Not browser yha node js per alert statement kaam nhi karega wahi agara browser per karege to ye wha kaam kar jayega to nodejsme error aayega
+
+
+console.log(3
+    +3); //code raedability should be high aaysa mat karna faltu ka
+
+ //dataTypes ka Example
+ let name ="Anish"
+ let age = 18
+ let isLoggedIn =false
+ 
+ //number=>range 2 to power 53
+ //bigint => baare paltform me use hota h like trading app 
+//string =""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol=> uniquenees Find karne ke Liye Symbol use karte h
+ 
+console.log(typeof "ansih");//string hota h iska dataype
+console.log(typeof null)// Object aata h iska dataTypes
+console.log(typeof undefined)//iska undefined heaatahdataTypes
+
+
+
