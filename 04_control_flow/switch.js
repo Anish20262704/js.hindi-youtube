d19c77c831ec7134ce1a53853ffffb6a85d10f13
Anish20262704 <152880188+Anish20262704@users.noreply.github.com>
2025-04-10 18:28:04 +0000

    More Discussion on Switch Case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {  //key-> wo value jo haamko har baar check karna h
+  
+//     case value:
+                        //ye basic Syntax h Switch Case ka to yaad rakhna
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+
+switch (month) {
+
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("feb");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
